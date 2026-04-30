e75b81d76c605a11db40ddcbb99357fa95292021
Karly K. <alternatives.sln@gmail.com>
2026-04-30 17:50:35 -0400

    Copying files to 04/1_dual1of2
    Saving current embedded POC code to 04/1_dual1of2

---
+++ VM/1_EmbeddedCPP/04/1_dual1of2/ESP32_WROVER__virtualMirror/ESP32_WROVER__virtualMirror.ino
@@ -0,0 +1,59 @@
+/**********************************************************************
+* Filename    : ESP32_WROVER__virtualMirror
+* Description : Make builtin led to blink when printing out heartbeat.
+* In this case we will be using the power of the 2 cores esp32 has.
+* Auther      : Alternatives Solutions
+* Modification: 2026/04/30
+**********************************************************************/
+#define LED_BUILTIN  2  
+#define DELAY_TIME   1000
+
+// Creating a "Handle" (an ID card for our task). It is optional for now
+TaskHandle_t MyTaskHandle;
+
+// Function for our Task on Core 0
+void MyWorkerFunc(void * pvParameters) {
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;) { // Workers need their own infinite loop
+    Serial.println("System Heartbeat: Core 0 is Healthy");
+    delay(DELAY_TIME);
+  }
+}
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // create a task that will be executed in the 
+  // MyWorkerFunc() function, with priority 1 
+  // and executed on core 0
+  xTaskCreatePinnedToCore(
+        MyWorkerFunc,  /* Task function. */
+        "Task1",       /* name of task. */
+        10000,         /* Stack size of task */
+        NULL,          /* parameter of the task */
+        1,             /* priority of the task */
+        &MyTaskHandle, /* Task handle to keep track of created task */
+        0              /* pin task to core 0 */
+  );           
+    
+
+
+  Serial.println("System initialized, up and running... ");
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED off (HIGH is the voltage level)
+  Serial.println("LED OFF");
+  delay(DELAY_TIME/2);                 // wait for a Defined delay time
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED ON, Active LOW
+  Serial.println("LED ON");
+  delay(DELAY_TIME/2);                 // wait for a Defined delay time
+}
+
